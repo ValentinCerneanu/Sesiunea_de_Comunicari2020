398cf661a85cb8bf70f68c93d151c4fb15a22b9d
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-05-05 20:58:41 +0300

    gdp ranking

--- ranking_script.py
+++ ranking_script.py
@@ -12,78 +12,103 @@ import requests
 import numpy as np
 import operator
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+def main():
+    errors = ''
 
-
-countries_population_df = pd.read_csv("countries_population.csv", header=None)
-countries_population = {}    
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
     
-for index, row in countries_population_df.iterrows():
-    countries_population[row[0]] = row[2]
-
-url = "https://api.covid19api.com/"
-countryUrl = url + "countries"
-byCountryUrl = url + "total/country/{0}"
-
-payload = {}
-headers= {}
-
-response = requests.request("GET", countryUrl, headers=headers, data = payload)
-
-country_ranking = {}
-
-df = pd.DataFrame(response.json())
-
-for index, row in df.iterrows():
-    try:
-        break
-        print(row['Slug'])
-    
-        response = requests.request("GET", byCountryUrl.format(row['Slug']), headers=headers, data = payload)
-        responseArray = np.array(response.json())
+    countries_population_df = pd.read_csv("countries_population.csv", header=None)
+    countries_population = {}    
         
-        if responseArray.size > 0: 
-            df = pd.DataFrame(response.json())
-            df.to_csv (row['Slug'] + '.csv', index = None)
+    for index, row in countries_population_df.iterrows():
+        countries_population[row[0]] = row[2]
+    
+    url = "https://api.covid19api.com/"
+    countryUrl = url + "countries"
+    byCountryUrl = url + "total/country/{0}"
+    
+    payload = {}
+    headers= {}
+    
+    response = requests.request("GET", countryUrl, headers=headers, data = payload)
+    
+    country_ranking = {}
+    
+    df = pd.DataFrame(response.json())
+    
+    for index, row in df.iterrows():
+        try:           
+            print(row['Slug'])
+        
+            response = requests.request("GET", byCountryUrl.format(row['Slug']), headers=headers, data = payload)
+            responseArray = np.array(response.json())
             
-            data = pd.read_csv(row['Slug'] + '.csv',header=None)
-            
-            T = data.iloc[1:,7:10]
-            X = data.iloc[1:,0:1]
-            for i in range(1, len(T)+1):
-                X.loc[i, 0] = i
-            
-            predict = pd.DataFrame()
-            for i in range(1, len(T)+1):
-                predict.loc[i, 0] = len(T)+i
-            
-            net = MLPRegressor(hidden_layer_sizes=(10, 10), activation='relu', solver='lbfgs') #perfect
-            
-            net.fit(X, T)
-            predictedData = net.predict(predict) 
-            if countries_population.get(row['Country']) > 0:
-                score = round(round(predictedData[99][0], 3) / countries_population.get(row['Country']) * 100, 4)
-                print(score)
-                country_ranking[row['Country']] = score
-            if row['Country'] == 'Canada':
-                print(predictedData[99][0])
-                print(countries_population.get(row['Country']))
-                print(X)
-                print(T)
-                break
+            if responseArray.size > 0: 
+                df = pd.DataFrame(response.json())
+                df.to_csv (row['Slug'] + '.csv', index = None)
                 
-    except Exception:
-        print()
+                data = pd.read_csv(row['Slug'] + '.csv',header=None)
                 
+                T = data.iloc[1:,7:10]
+                X = data.iloc[1:,0:1]
+                for i in range(1, len(T)+1):
+                    X.loc[i, 0] = i
+                
+                predict = pd.DataFrame()
+                for i in range(1, len(T)+1):
+                    predict.loc[i, 0] = len(T)+i
+                
+                net = MLPRegressor(hidden_layer_sizes=(10, 10), activation='relu', solver='lbfgs') #perfect
+                
+                net.fit(X, T)
+                predictedData = net.predict(predict) 
+                if countries_population.get(row['Country']) > 0:
+                    score = round(round(predictedData[99][0], 3) / countries_population.get(row['Country']) * 100, 4)
+                    country_ranking[row['Country']] = score
+                    
+        except Exception as e:
+            errors = errors + str(e)
+                    
+    
+    sorted_country_ranking = sorted(country_ranking.items(), key=operator.itemgetter(1))
+    print(sorted_country_ranking)
 
-sorted_country_ranking = sorted(country_ranking.items(), key=operator.itemgetter(1))
-print(sorted_country_ranking)
+    gdp_and_percentage_services = pd.read_csv("gdp_and_percentage_services.csv", header=None)
+    countries_gdp_services = gdp_and_percentage_services.iloc[:,0:3]
+    print(countries_gdp_services)
+    print(len(countries_gdp_services))
+    country_gdp_ranking = {};
+    for index, row in countries_gdp_services.iterrows():
+        try:
+            if isNotNaN(row[1]) and isNotNaN(row[2]):
+                score = float(row[1]) * 1/100 * float(row[2])
+                country_gdp_ranking[row[0]] = score
+                sorted_country_ranking[row[0]] = score
+        except Exception as e:
+            errors = errors + str(e)
 
-gdp_and_percentage_services = pd.read_csv("gdp_and_percentage_services.csv", header=None)
-countries_gdp_services = gdp_and_percentage_services.iloc[:,0:3]
-print(countries_gdp_services)
+    
+    sorted_country_gdp_ranking = sorted(country_gdp_ranking.items(), key=operator.itemgetter(1))
+    
+    
+    print(sorted_country_ranking)
+
+main()
+
+def isNotNaN(num):
+    return num == num
+
+def is_float(value):
+  try:
+    float(value)
+    return True
+  except:
+    return False
+        
+
+    
 
 
 
