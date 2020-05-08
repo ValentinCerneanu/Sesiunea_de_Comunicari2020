f663cedf50e2922bef95872966aebcf40974c540
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-05-08 11:09:36 +0300

    10 days prediction

--- ranking_script.py
+++ ranking_script.py
@@ -41,6 +41,7 @@ def main():
     for index, row in df.iterrows():
         try:           
             print(row['Slug'])
+            print(index)
         
             response = requests.request("GET", byCountryUrl.format(row['Slug']), headers=headers, data = payload)
             responseArray = np.array(response.json())
@@ -57,7 +58,7 @@ def main():
                     X.loc[i, 0] = i
                 
                 predict = pd.DataFrame()
-                for i in range(1, len(T)+1):
+                for i in range(1, 11):
                     predict.loc[i, 0] = len(T)+i
                 
                 net = MLPRegressor(hidden_layer_sizes=(10, 10), activation='relu', solver='lbfgs') #perfect
@@ -65,35 +66,38 @@ def main():
                 net.fit(X, T)
                 predictedData = net.predict(predict) 
                 if countries_population.get(row['Country']) > 0:
-                    score = round(round(predictedData[99][0], 3) / countries_population.get(row['Country']) * 100, 4)
+                    score = round(round(predictedData[9][0], 3) / countries_population.get(row['Country']) * 100, 4)
                     country_ranking[row['Country']] = score
-                    
+            
         except Exception as e:
             errors = errors + str(e)
+        if index > 15:
+            break
+    print(errors)
                     
-    
+    print(country_ranking)
     sorted_country_ranking = sorted(country_ranking.items(), key=operator.itemgetter(1))
     print(sorted_country_ranking)
 
     gdp_and_percentage_services = pd.read_csv("gdp_and_percentage_services.csv", header=None)
     countries_gdp_services = gdp_and_percentage_services.iloc[:,0:3]
     print(countries_gdp_services)
-    print(len(countries_gdp_services))
+   
     country_gdp_ranking = {};
     for index, row in countries_gdp_services.iterrows():
         try:
             if isNotNaN(row[1]) and isNotNaN(row[2]):
                 score = float(row[1]) * 1/100 * float(row[2])
                 country_gdp_ranking[row[0]] = score
-#                sorted_country_ranking[row[0]] = score
+                sorted_country_ranking[row[0]] = score
         except Exception as e:
             errors = errors + str(e)
 
-    
+    print(country_gdp_ranking)
     sorted_country_gdp_ranking = sorted(country_gdp_ranking.items(), key=operator.itemgetter(1))
     
     
-    print(sorted_country_ranking)
+#    print(sorted_country_ranking)
 
 main()
 
@@ -105,13 +109,4 @@ def is_float(value):
     float(value)
     return True
   except:
-    return False
-        
-
-    
-
-
-
-
-
-
+    return False 
